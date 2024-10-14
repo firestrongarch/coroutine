56b65024df3961611c45bd9ccc22a97d25227d40
fu <1766423721@qq.com>
2024-10-14 14:44:19 +0800

    模板形参需要默认构造函数

--- demo/sequence_generator.cpp
+++ demo/sequence_generator.cpp
@@ -3,34 +3,36 @@
 #include <utility>
 
 struct Generator{
-    using promise_type = Generator;
-    // 开始执行时不挂起，执行到第一个挂起点
-    std::suspend_never initial_suspend() { return {}; };
+    struct promise_type{
+                // 开始执行时不挂起，执行到第一个挂起点
+        std::suspend_never initial_suspend() { return {}; };
 
-    // // 执行结束后不需要挂起
-    // std::suspend_never final_suspend() noexcept { return {}; }
+        // // 执行结束后不需要挂起
+        // std::suspend_never final_suspend() noexcept { return {}; }
 
-    // 总是挂起，让 Generator 来销毁
-    std::suspend_always final_suspend() noexcept { return {}; }
+        // 总是挂起，让 Generator 来销毁
+        std::suspend_always final_suspend() noexcept { return {}; }
 
-    // 为了简单，我们认为序列生成器当中不会抛出异常，这里不做任何处理
-    void unhandled_exception() { }
+        // 为了简单，我们认为序列生成器当中不会抛出异常，这里不做任何处理
+        void unhandled_exception() { }
 
-    // 构造协程的返回值类型
-    Generator get_return_object() {
-        return Generator{std::coroutine_handle<promise_type>::from_promise(*this)};
-    }
+        // 构造协程的返回值类型
+        Generator get_return_object() {
+            return Generator{std::coroutine_handle<promise_type>::from_promise(*this)};
+        }
+        
+        // 传值的同时要挂起，值存入 value 当中
+        std::suspend_always await_transform(int value) {
+            this->value_ = value;
+            return {};
+        }
+        int value_;
+
+        // 没有返回值
+        void return_void() { }
+    };
+
     std::coroutine_handle<promise_type> h_;
-
-    // 传值的同时要挂起，值存入 value 当中
-    std::suspend_always await_transform(int value) {
-        this->value_ = value;
-        return {};
-    }
-    int value_;
-
-    // 没有返回值
-    void return_void() { }
 
     int next() {
         h_.resume();
